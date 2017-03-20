6d386edaebc275787bf194c0466c4d01843c16d1
ThefuckingChamp <chrklo@hotmail.com>
2017-03-20 08:33:57 +0100

    Add files via upload

---
+++ FirstScanner/FirstScanner/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstScanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Scanner scanner = new Scanner();
+
+            scanner.Scan();
+            scanner.printTokens();
+
+        }
+    }
+}
