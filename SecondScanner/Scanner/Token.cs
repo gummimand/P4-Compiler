8e95e6a13ec2c709574038c3479e5a66e6f0a1c8
ThefuckingChamp <chrklo@hotmail.com>
2017-03-22 12:31:28 +0100

    Add files via upload

---
+++ SecondScanner/Scanner/Token.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstScanner
+{
+    class Token
+    {
+        public string content;
+        public string type;
+
+        public Token(string _content)
+        {
+            content = _content;
+        }
+
+        public Token(string _content, string _type)
+        {
+            content = _content;
+            type = _type;
+        }
+    }
+}
